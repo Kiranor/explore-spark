d76b123c37e123e6cbe739ef95e04c4701d0b7a4
Tatiana Varnavskaya <varnavskaya@gnivc.ru>
2024-12-11 17:58:41 +0300

    change tasks

--- src/main/scala/sparkEssentials/Aggregations.scala
+++ src/main/scala/sparkEssentials/Aggregations.scala
@@ -64,42 +64,4 @@ object Aggregations extends App {
     )
     .orderBy(col("Avg_Rating"))
 
-
-  /**
-    * Задание 4
-    *
-    * 1. Посчитать суммарный доход с фильмов по всем параметрам Total_Gross = US_Gross + Worldwide_Gross" + US_DVD_Sales
-    * 2. Посчитать, сколько есть уникальных режиссеров в предоставленном наборе данных
-    * 3. Показать среднее (mean) and стандартное отклонение (standard deviation) по колонке US_Gross
-    * 4. Посчитать средний рейтинг (IMDB) и среднее US_Gross для каждого режиссера
-    */
-
-
-  // 1
-  moviesDF
-    .select((col("US_Gross") + col("Worldwide_Gross") + col("US_DVD_Sales")).as("Total_Gross"))
-    .select(sum("Total_Gross"))
-    .show()
-
-  // 2
-  moviesDF
-    .select(countDistinct(col("Director")))
-    .show()
-
-  // 3
-  moviesDF.select(
-    mean("US_Gross"),
-    stddev("US_Gross")
-  ).show()
-
-  // 4
-  moviesDF
-    .groupBy("Director")
-    .agg(
-      avg("IMDB_Rating").as("Avg_Rating"),
-      sum("US_Gross").as("Total_US_Gross")
-    )
-    .orderBy(col("Avg_Rating").desc_nulls_last)
-    .show()
-
 }
